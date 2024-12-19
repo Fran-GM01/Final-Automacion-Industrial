8c6584cba5b1cae5a2a28671b3ab7095f7a86f27
Francisco García Migliaro <franmigliaro@gmail.com>
2024-12-19 12:58:11 -0300

    Add files via upload
    getLinesCoordiantes ahora devuelve a distancia de los extremos de la linea respescto a la esquina suroeste.

---
+++ main.m
@@ -0,0 +1,17 @@
+clear
+clc
+close all
+
+foto='warped3.jpg';
+im=iread(foto,'double');
+
+frameCorner=getCorners(im);
+[correctedImg,finalCorners]=warpImage(im,frameCorner);
+[endPoints,realDsitance]=getLineCoordinates(correctedImg,finalCorners);
+
+%realDistance es la distancia de los puntos a la esquina suroeste, en [m].
+
+figure
+idisp(correctedImg)
+hold on
+plot(endPoints(:,1),endPoints(:,2),'*b')
